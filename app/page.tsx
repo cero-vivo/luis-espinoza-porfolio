1dcb3f238349989f1a8a85fed0be3581f8738dd4
Luis Espinoza <luis.espinoza.nav@live.com>
2024-06-02 22:04:25 -0300

    fix header

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Landing } from "@/components/pages/landing/landing";
+import { Skills } from "@/components/pages/skills/skills";
 
 export default function Home() {
 	return (
 		<main>
 			<Landing />
+			<Skills/>
 		</main>
   )
 }
---
+++ components/pages/skills/skills.tsx
@@ -0,0 +1,9 @@
+import { Frame } from '@/components/basic/frame/frame'
+import React from 'react'
+
+export const Skills = () => {
+	return (
+		<Frame>
+		</Frame>
+	)
+}
