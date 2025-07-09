168e88006b57788fc0b15277d42a150003bedcea
Luis <luis.espinoza@bizland.tech>
2025-07-09 00:52:59 -0300

    fix footer

--- components/shared/footer/footer.tsx
+++ components/shared/footer/footer.tsx
@@ -2,14 +2,13 @@
 
 import React from 'react'
 import { EmailIcon } from '@/components/basic/icons/email-icon'
-import { useLandingStore } from '@/model/landing-store'
-import { colors } from '@/theme/colors'
-import styles from "./footer.module.css"
-import { Paragraph } from '@/components/basic/paragraph/paragraph'
 import { WhatsappIcon } from '@/components/basic/icons/whatsapp-icon'
 import { PhoneIcon } from '@/components/basic/icons/phone-icon'
 import ExternalLinkIcon from '@/components/basic/icons/external-link-icon'
+import { Paragraph } from '@/components/basic/paragraph/paragraph'
+import { useLandingStore } from '@/model/landing-store'
 import { useTranslations } from 'next-intl'
+import styles from "./footer.module.css"
 
 export const Footer = () => {
 
@@ -19,20 +18,21 @@ const { sendMeEmail, callMe, sendMeWhatsapp, openLinkedin } = useLandingStore()
 
 	return (
 		<footer className={styles.footer}>
-			<span className={styles.infoRow}>
-				<EmailIcon color={colors.white} width={20} height={20} onClick={sendMeEmail} classes={styles.contactIcon} />
-				<Paragraph variant='regular' text={t("my_email")} classes={styles.text} onClick={sendMeEmail}/>
-			</span>
-			<span className={styles.infoRow}>
-				<WhatsappIcon color={colors.white} width={20} height={20} onClick={sendMeWhatsapp} classes={styles.contactIcon} />
-				<PhoneIcon color={colors.white} width={20} height={20} onClick={callMe} classes={styles.contactIcon} />
-				<Paragraph variant='regular' text={t("my_phone_number")} classes={styles.text} onClick={callMe} />
-			</span>
-			<span className={styles.infoRow} onClick={openLinkedin}>
-				<Paragraph variant='regular' text={t("linkedin_label")} classes={styles.text} />
-				<ExternalLinkIcon color={colors.white} width={14} height={14} onClick={openLinkedin} classes={styles.contactIcon} />
-			</span>
-			<Paragraph variant='regular' text={new Date()?.getFullYear()?.toString()} classes={styles.date} />
+			<div className={styles.iconRow}>
+				<a className={styles.iconLink} onClick={sendMeEmail} href="#">
+					<EmailIcon color="currentColor" /> {t('email_short')}
+				</a>
+				<a className={styles.iconLink} onClick={sendMeWhatsapp} href="#">
+					<WhatsappIcon color="currentColor" /> WhatsApp
+				</a>
+				<a className={styles.iconLink} onClick={callMe} href="#">
+					<PhoneIcon color="currentColor" /> {t('call')}
+				</a>
+				<a className={styles.iconLink} onClick={openLinkedin} href="#">
+					<ExternalLinkIcon color="currentColor" width={20} height={20} /> LinkedIn
+				</a>
+			</div>
+			<Paragraph variant='regular' text={`© ${new Date().getFullYear()}`} classes={styles.copy} />
 		</footer>
 	)
 }
